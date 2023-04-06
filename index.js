cd61905cb2c80061faec655f63d7b3e0e40eace0
Jacob Rathbun <jrathofbun@gmail.com>
2023-04-05 23:28:15 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+//declare companyName constant variable as "Scuber"
+const companyName = 'Scuber';
+
+//declare variable mostProfitableNeighbordhood using let
+let mostProfitableNeighborhood = 'Chelsea';
+
+//declare companyCeo variable
+let companyCeo = 'Susan Smith';
